c758e249929f05df1fcbc89ddc4595e4c01c2c3e
sonudeepa3011-afk <sonudeepa3011@gmail.com>
2026-07-19 00:15:17 +0530

    Update sarkariresult.py

--- scraper/sarkariresult.py
+++ scraper/sarkariresult.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-API_URL = "https://www.sarkariresult.com/wp-json/wp/v2/posts?per_page=20&_embed""
+API_URL = "https://www.sarkariresult.com/wp-json/wp/v2/posts?per_page=20&_embed"
 
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0 Safari/537.36",
