6b51e3b367628dfca890a56f850d950e043fbb6b
sonudeepa3011-afk <sonudeepa3011@gmail.com>
2026-07-19 01:16:07 +0530

    Update gemini_ai.py

--- gemini_ai.py
+++ gemini_ai.py
@@ -3,7 +3,7 @@ import google.generativeai as genai
 
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
-model = genai.GenerativeModel("gemini-flash-latest")
+model = genai.GenerativeModel("gemini-1.5-flash")
 
 
 def generate_news(title, description):
